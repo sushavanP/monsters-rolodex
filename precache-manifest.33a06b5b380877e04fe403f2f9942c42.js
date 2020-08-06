5d29e35d12db0beebebabd387a159758ebf4df09
Sushavan Paul <sushavan.paul.524@gmail.com>
2020-08-06 22:08:10 +0530

    Updates

---
+++ precache-manifest.33a06b5b380877e04fe403f2f9942c42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d217ed2c38d0150d504ad2a4028e8dab",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "08bce6129ed835a79cd2",
+    "url": "/monsters-rolodex/static/css/main.11550a55.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monsters-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08bce6129ed835a79cd2",
+    "url": "/monsters-rolodex/static/js/main.8f82c40e.chunk.js"
+  },
+  {
+    "revision": "dfba82531f669c6858b0",
+    "url": "/monsters-rolodex/static/js/runtime-main.589ddcef.js"
+  }
+]);
